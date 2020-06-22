56838b265d18b26ee48d53b8a08be4f3a060da05
An Pham <anpham148@gmail.com>
2020-06-22 11:22:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     new_arr = array.join
   elsif array.count == 2
     new_arr = array.join(" and ")
-
+  else
     new_arr = array.insert(-2, "and").join(", ")
   end
   new_arr
