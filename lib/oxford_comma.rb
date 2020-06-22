efd756007a17783de6570f43648ae50b0aabbdb4
An Pham <anpham148@gmail.com>
2020-06-22 11:12:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   counter = 1
   array.each do |element|
-    if counter = 1
+    if counter == 1
       array.join
-    elsif counter = 2
+    elsif counter == 2
       array.join(" and ")
     end
+    counter += 1
   end
 end
